6e97a0c72f8612bee804a3577f6d709da7431e4a
Amir Joudaki <amir.judaki@gmail.com>
2025-05-19 18:41:55 +0200

    updating cloning experiments script

--- scripts/gen_cloning_experiment_grid.sh
+++ scripts/gen_cloning_experiment_grid.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Define arrays for each parameter that varies
-models=("mlp" "cnn" "vit" "resnet")
+models=("mlp" "cnn" "resnet" "vit")
 normalizations=("batch" "layer" "none")
 dropout_values=("0")
-seeds=("41" "42" "43")
+seeds=("40" "41" "42" "43" "44")
 optimizers=("noisysgd")
-noise_scales=("0.01" "0.02")
-noise_decays=("0" "0.9" "0.99" "0.999")
+noise_scales=("0.01" "0.02" "0.05")
+noise_decays=("0" "0.9" "0.99" "0.999" "1.0")
 learning_rates=("0.01")  # Added learning rate grid
 
 # Fixed parameters
 dataset="cifar10"
-initial_epochs="10"
-epochs_per_expansion="100"
+num_workers="2"
+initial_epochs="20"
+epochs_per_expansion="500"
 wandb_tags="[main,cloning]"
 
 # Counter for experiments
@@ -23,10 +24,10 @@ total=$((${#models[@]} * ${#normalizations[@]} * ${#dropout_values[@]} * ${#seed
 echo "Starting $total individual experiment runs..."
 
 # Loop through all combinations
-for model in "${models[@]}"; do
-  for norm in "${normalizations[@]}"; do
-    for dropout in "${dropout_values[@]}"; do
-      for seed in "${seeds[@]}"; do
+for seed in "${seeds[@]}"; do
+  for model in "${models[@]}"; do
+    for norm in "${normalizations[@]}"; do
+      for dropout in "${dropout_values[@]}"; do
 	for optimizer in "${optimizers[@]}"; do
         for lr in "${learning_rates[@]}"; do  # Added learning rate loop
 	for noise_scale in "${noise_scales[@]}"; do
@@ -42,6 +43,7 @@ for model in "${models[@]}"; do
 	    training.initial_epochs=$initial_epochs \
             training.epochs_per_expansion=$epochs_per_expansion \
             training.seed=$seed \
+            training.num_workers=$num_workers \
 	    optimizer=$optimizer \
 	    optimizer.noise_decay=$noise_decay \
 	    optimizer.noise_scale=$noise_scale \
@@ -49,7 +51,7 @@ for model in "${models[@]}"; do
             wandb_tags=$wandb_tags "
           
           echo gpujob submit \"$CMD\"
-	  gpujob submit "$CMD"
+	  # gpujob submit --memory 20 "$CMD"
 
           echo ""
 
